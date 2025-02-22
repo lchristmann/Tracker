ce6d4446083a347f0a634d89db674eaf039e46b0
Leander Christmann <leander.christmann@proton.me>
2025-02-22 13:41:40 +0100

    feature: request location permissions + check they've been granted

---
+++ app/src/main/java/com/lchristmann/tracker/LocationUtils.kt
@@ -0,0 +1,21 @@
+package com.lchristmann.tracker
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.content.ContextCompat
+
+class LocationUtils(val context: Context) {
+
+    fun hasLocationPermission(context: Context):Boolean {
+
+        return ContextCompat.checkSelfPermission(
+            context,
+            Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                &&
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+}
--- app/src/main/java/com/lchristmann/tracker/MainActivity.kt
+++ app/src/main/java/com/lchristmann/tracker/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.lchristmann.tracker
 
+import android.Manifest
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.lchristmann.tracker.ui.theme.TrackerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,11 +26,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
                 }
             }
         }
@@ -31,17 +39,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LocationDisplay(
+    locationUtils: LocationUtils,
+    context: Context
+) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrackerTheme {
-        Greeting("Android")
+
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult = { permissions ->
+            if (permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+                && permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true) {
+                // I have access to location
+            } else {
+                // Ask for permission
+            }
+        }
+    )
+
+    Column(modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        Text(text = "Location not available")
+
+        Button(onClick = {
+            if (locationUtils.hasLocationPermission(context)) {
+                // Permission already granted, update the location
+            } else {
+                // Request location permission
+            }
+        }) {
+            Text(text = "Get Location")
+        }
     }
 }
